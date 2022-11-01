6402e45a40303997a5a3e5c1971008fcaa202af6
Jaytee <Padilla.Jaytee@gmail.com>
2022-10-31 23:21:33 -0700

    Completed article-card layout and added placeholder data

--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -1,67 +1,42 @@
-import { Box, Image, Badge } from '@chakra-ui/react';
-import { StarIcon } from '@chakra-ui/icons'
+import { Box } from '@chakra-ui/react';
+import { TriangleUpIcon } from '@chakra-ui/icons';
 
-export const ArticleCard = () => {
-  const property = {
-    imageUrl: 'https://bit.ly/2Z4KKcF',
-    imageAlt: 'Rear view of modern home with pool',
-    beds: 3,
-    baths: 2,
-    title: 'Modern home in city center in the heart of historic Los Angeles',
-    formattedPrice: '$1,900.00',
-    reviewCount: 34,
-    rating: 4,
+export const ArticleCard = (props: any) => {
+  const tempArticleData = {
+    title: "Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae",
+    time: "2 weeks ago",
+    by: "Jaysteez",
+    score: "707",
+    descendants: "808"
   }
 
   return (
-    <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-      <Image src={property.imageUrl} alt={property.imageAlt} />
+    <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
 
-      <Box p='6'>
-        <Box display='flex' alignItems='baseline'>
-          <Badge borderRadius='full' px='2' colorScheme='teal'>
-            New
-          </Badge>
-          <Box
-            color='gray.500'
-            fontWeight='semibold'
-            letterSpacing='wide'
-            fontSize='xs'
-            textTransform='uppercase'
-            ml='2'
-          >
-            {property.beds} beds &bull; {property.baths} baths
-          </Box>
-        </Box>
+      <Box display="flex" justifyContent="space-evenly" p="6">
 
-        <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h4'
-          lineHeight='tight'
-          noOfLines={1}
-        >
-          {property.title}
+        <Box mr="4">
+          <TriangleUpIcon />
         </Box>
 
         <Box>
-          {property.formattedPrice}
-          <Box as='span' color='gray.600' fontSize='sm'>
-            / wk
+          <Box
+            fontWeight="bold"
+            letterSpacing="wide"
+            as="h4"
+            lineHeight="tight"
+            fontSize="lg"
+            mb="3"
+          >
+            {tempArticleData.title}
           </Box>
-        </Box>
 
-        <Box display='flex' mt='2' alignItems='center'>
-          {Array(5)
-            .fill('')
-            .map((_, i) => (
-              <StarIcon
-                key={i}
-                color={i < property.rating ? 'teal.500' : 'gray.300'}
-              />
-            ))}
-          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-            {property.reviewCount} reviews
+          <Box>
+            {tempArticleData.time}
+          </Box>
+
+          <Box>
+            {tempArticleData.score} points | {tempArticleData.descendants} comments | by&nbsp;{tempArticleData.by}
           </Box>
         </Box>
       </Box>
